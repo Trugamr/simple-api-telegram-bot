82fd481dfc2935d5b9ab34509e663d34c534c20c
Trugamr <6trugamr9@gmail.com>
2020-07-19 20:12:18 +0530

    added random fortune, dog and cat commands

--- bot.js
+++ bot.js
@@ -2,11 +2,20 @@
 require('dotenv').config()
 
 const Telegraf = require('telegraf')
+const { fortune, randomCat, dogBreeds, randomDog } = require('./commands')
 
 const { TELEGRAM_BOT_TOKEN } = process.env
 
 const bot = new Telegraf(TELEGRAM_BOT_TOKEN)
 
-bot.start(ctx => ctx.reply('Hello !'))
+bot.start(ctx => {
+  ctx.reply('Hello !')
+})
+
+// Custom commands
+bot.command('fortune', fortune)
+bot.command('cat', randomCat)
+bot.command('dogbreeds', dogBreeds)
+bot.command('dog', randomDog)
 
 bot.launch()
---
+++ commands.js
@@ -0,0 +1,100 @@
+const axios = require('axios')
+
+const dogBreedsArr = require('./data/dog-breeds')
+
+// Utilty functions
+// Get spaced arguments as array
+const getArgs = text => text.split(' ').splice(1)
+
+// Get random fortune
+const getFortune = () => axios.get('http://yerkee.com/api/fortune')
+
+exports.fortune = (ctx, next) => {
+  getFortune()
+    .then(response => response.data)
+    .then(data => {
+      ctx.reply(`\`${data['fortune']} \``, {
+        parse_mode: 'MarkdownV2'
+      })
+    })
+    .catch(error => {
+      console.log('FORTUNE FAILED: ', error)
+      ctx.reply('Failed to get fortune.')
+    })
+}
+
+// Get random cat image
+const getRandomCat = async () => {
+  try {
+    const response = await axios.get(
+      'https://api.thecatapi.com/v1/images/search'
+    )
+    const catURL = response.data[0].url
+    return catURL
+  } catch (error) {
+    throw error
+  }
+}
+
+const getCatWithText = text => `https://cataas.com/cat/says/${text}?size=80`
+
+exports.randomCat = async (ctx, next) => {
+  const args = getArgs(ctx.message.text)
+  try {
+    if (args.length) {
+      ctx.replyWithPhoto(getCatWithText(args.join('')))
+    } else {
+      const catURL = await getRandomCat()
+      ctx.replyWithPhoto(catURL)
+    }
+  } catch (error) {
+    ctx.reply('Failed to get to a cat :(')
+  }
+}
+
+// Get dog breeds list
+exports.dogBreeds = (ctx, next) => {
+  const message = dogBreedsArr.map(breed => `- ${breed}`).join('\n')
+  ctx.reply(message)
+}
+
+// Get random dog images
+const getRandomDog = async (breed = 'akita') => {
+  try {
+    const response = await axios.get(
+      `https://dog.ceo/api/breed/${breed}/images/random`
+    )
+    const dogURL = response.data['message']
+    return dogURL
+  } catch (error) {
+    throw error
+  }
+}
+
+exports.randomDog = async (ctx, next) => {
+  const args = getArgs(ctx.message.text)
+  if (args.length == 0) {
+    ctx.reply('You must provide dog breed as second argument.')
+    return
+  }
+
+  const inputBreed = args[0]
+  if (dogBreedsArr.includes(inputBreed)) {
+    const dogURL = await getRandomDog(inputBreed)
+    console.log(dogURL)
+    ctx.replyWithPhoto(dogURL)
+  } else {
+    const suggestions = dogBreedsArr.filter(breed =>
+      breed.startsWith(inputBreed)
+    )
+
+    let message = 'Did you mean:\n'
+    message += suggestions.map(breed => `- ${breed}`).join('\n')
+
+    if (suggestions.length == 0) {
+      ctx.reply("Can't find the specified breed")
+    } else {
+      ctx.reply(message)
+    }
+  }
+}
